144ebf634aa68cb0e0507c29cb9ae6d2971ad0e1
HYDRA <hydrxrd@gmail.com>
2026-03-09 23:23:27 -0300

    Update StakeSection.tsx

--- src/components/StakeSection.tsx
+++ src/components/StakeSection.tsx
@@ -38,9 +38,11 @@ const StakeSection = () => {
             title="staking-widget"
             src="https://radix.defiplaza.net/widget/staking/resource_rdx1t4kc2yjdcqprwu70tahua3p8uwvjej9q3rktpxdr8p5pmcp4almd6r?theme=blue"
             width="100%"
-            scrolling="no"
             height="1000"
-            style={{ border: 0, margin: "0 auto", display: "block", minWidth: 375, colorScheme: "normal" }}
+            scrolling="no"
+            frameBorder="0"
+            className="w-full h-[1000px] block"
+            style={{ border: 0, margin: "0 auto", minWidth: 375, colorScheme: "normal" }}
           />
         </motion.div>
       </div>
